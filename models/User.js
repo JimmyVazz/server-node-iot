5e4387c76c256a404384d94b70d9f2a0a5073c9d
JimmyVazz <cjvasquez_98@hotmail.com>
2020-11-23 15:03:19 -0600

    primer commit :fire: y routes de prueba added

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: String,
+    ocupacion: String,
+    edad: Number,
+    email: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
